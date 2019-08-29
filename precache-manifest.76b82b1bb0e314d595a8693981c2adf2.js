568be4febfe1890ca9bfea4da4372a666ee143f5
Ncf4n1 <llvinin@hotmail.com>
2019-08-29 11:15:04 -0600

    Updates

---
+++ precache-manifest.76b82b1bb0e314d595a8693981c2adf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60610b53b40f84bafa4922a0b42f7722",
+    "url": "/React-Recipe-Organizer/index.html"
+  },
+  {
+    "revision": "9b3c823ac89726e02031",
+    "url": "/React-Recipe-Organizer/static/css/main.aa128bfb.chunk.css"
+  },
+  {
+    "revision": "4b53d01f5383fdf79fbd",
+    "url": "/React-Recipe-Organizer/static/js/2.4eecb4df.chunk.js"
+  },
+  {
+    "revision": "9b3c823ac89726e02031",
+    "url": "/React-Recipe-Organizer/static/js/main.1109f4e0.chunk.js"
+  },
+  {
+    "revision": "6fa7eb34edcc1b3ca619",
+    "url": "/React-Recipe-Organizer/static/js/runtime~main.9d999890.js"
+  }
+]);
