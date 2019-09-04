017e93eeceb27296a11d44826c9070678d2032ac
Ncf4n1 <llvinin@hotmail.com>
2019-09-04 10:57:34 -0600

    finished change recipe buttons functionality and manually added recipes

--- src/App.js
+++ src/App.js
@@ -20,43 +20,166 @@ class Recipe extends React.Component {
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.changeRecipe = this.changeRecipe.bind(this);
     this.state = {
       recipes: [
         {
           title: 'Salsa',
           ingredients: [
-            '1/2 cup tomato',
-            '1/2 cup green, yellow, red peppers each',
-            '1/4 cup cilantro'
+            '2 14oz (or 1 26oz) can(s) diced tomatoes',
+            '1 small can tomato sauce',
+            '1 can El pato duck sauce',
+            '1/2 chopped onion',
+            'Chopped cilantro',
+            'Garlic salt'
           ],
           steps: [
-            'Cut up tomato into medium bowl',
-            'Cut up peppers into bowl',
-            'Dice cilantro and add to bowl',
-            'Mix and serve with your favorite chips'
+            'Mix tomatoes, tomato sauce, and El pato into medium bowl',
+            'Mix in onions, cilantro, and garlic salt into bowl',
+            'Serve with tortilla chips'
           ]
         },
         {
-          title: 'Toast',
+          title: 'Banana Bread',
           ingredients: [
-            'Bread',
-            'Toaster'
+            '2 eggs',
+            '1/2 cup sugar',
+            '1 cup sour cream',
+            '1 teaspoon soda',
+            '1/4 teaspoon salt',
+            '1 cup mashed bananas',
+            '2 cups flour',
+            '1 teaspoon baking powder',
+            'Nuts'
           ],
           steps: [
-            'Put bread in toast',
-            'Run toaster',
-            'Eat'
+            'Mix ingredients into baking bowl',
+            'Bake at 350 degrees for 50-55 minutes',
+            'Mixture makes one medium loaf'
+          ]
+        },
+        {
+          title: 'Mini Caramel Rolls',
+          ingredients: [
+            '1/3 cup packed brown sugar',
+            '1/3 cup cubed butter',
+            '2 tablespoons light corn syrup',
+            '1-1/2 teaspoons 2% milk',
+            '1 tube (8oz) refrigerated crescent rolls',
+            '2 teaspoons Imperial Sugar / Dixie Crystals Granulated Sugar',
+            '1/2 teaspoon ground cinnamon'
+          ],
+          steps: [
+            'Combine brown sugar, butter, corn syrup, and milk into small saucepan',
+            'Cook and stir over medium heat until butter melts and sugar is dissolved',
+            'Pour mix into greased 9 inch pie plate',
+            'Separate crescent dough into four rectangles and gently press to seal',
+            'Combine sugar and cinnamon into a small bowl and sprinkle over rectangles',
+            'Roll up rectangles jelly-style starting with the long side and pinch seams to seal',
+            'Cut each rectangles into nine slices and place cut side down in ready pie plate',
+            'Bake at 375 degrees for 15-18 minutes or until golden',
+            'Let cool in pie plate for 1 minute before flipping onto serving plate',
+            'This recipe yields 12 rolls'
+          ]
+        },
+        {
+          title: 'Buttermilk-Pineapple Carrot Cake',
+          ingredients: [
+            '2 cups all-purpose flour',
+            '2 cups sugar',
+            '2 teaspoons baking soda',
+            '1-1/2 teaspoons ground cinnamon',
+            '1 teaspoon baking powder',
+            '1/4 teaspoon salt',
+            '2 cups finely shredded carrots',
+            '1/4 cup buttermilk or sour milk',
+            '1/4 cup cooking oil',
+            '1 8-1/4oz can crushed pineapple, drained',
+            '1 cup chopped walnuts',
+            '3 eggs',
+            '1/2 cup coconut',
+            '1 teaspoon vanilla',
+            '1 recipe Buttermilk Glaze',
+            '1 recipe Cream Cheese Frosting',
+            '1/2 cup chopped walnuts (topping)'
+          ],
+          steps: [
+            'Grease 13x9x2 inch baking pan or grease and flour two 9x1-1/2 inch round baking pans',
+            'Combine flour, sugar, baking soda, cinnamon, baking powder, and salt in a large mixing bowl',
+            'Add shredded carrots, buttermilk or sour milk, cooking oil, drained pineapple, 1 cup nuts, eggs, coconut, and vanilla',
+            'Stir until combined and spread into baking pan(s)',
+            'Bake at 350 degrees for 40-45 minutes or until cake springs when touched',
+            'Pour Buttermilk Glaze over top of cake',
+            'Cool layers of cake in pans on wire rack for 15 minutes',
+            'Remove cakes and cool on racks',
+            'Frost with Cream Cheese Frosting and sprinkle with 1/2 cup of nuts'
+          ]
+        },
+        {
+          title: 'Buttermilk Glaze',
+          ingredients: [
+            '1/2 cup sugar',
+            '1/4 cup buttermilk or sour milk',
+            '1/4 cup butter',
+            '2 teaspoons light-colored corn syrup',
+            '1/2 teaspoon vanilla'
+          ],
+          steps: [
+            'Combine sugar, buttermilk or sour milk, butter, and corn syrup in a medium saucepan',
+            'Bring to boiling, then reduce heat',
+            'Cook and stir for 4 minutes',
+            'Remove saucepan from heat and stir in vanilla'
+          ]
+        },
+        {
+          title: 'Cream Cheese Frosting',
+          ingredients: [
+            '8oz cream cheese',
+            '1/4 cup butter',
+            '1 teaspoon vanilla',
+            '1 16oz package (4 cups) powdered sugar'
+          ],
+          steps: [
+            'Mix ingredients at room temperature into small bowl',
+            'Put in refrigerator overnight to let cool',
+            'Spread on your favorite dessert'
+          ]
+        },
+        {
+          title: 'Crab Dip',
+          ingredients: [
+            '1 8oz package softened cream cheese',
+            '1/2 cup mayonnaise',
+            '1/2 cup sliced green onion',
+            '1/2 cup chopped celery',
+            '2 tablespoons lemon juice',
+            '1 package (8oz) crab meat, chopped',
+          ],
+          steps: [
+            'Mix ingredients together in serving bowl',
+            'Chill several hours in refrigerator',
+            'Serve and enjoy'
           ]
         }
-      ]
+      ],
+      currentRecipe: null
     }
   }
   
-  changeRecipe(recipe) {
+  changeRecipe(recipeButton) {
+    const recipeTitle = recipeButton.target.id.split('_')[0];
+    const recipesByTitle = this.state.recipes.filter((recipe) => recipe.title === recipeTitle);
+    if (recipesByTitle.length > 0)
+    {
+      this.setState({
+        currentRecipe: recipesByTitle[0]
+      });
+    }
   }
   
   render() {
+    let currentRecipe = this.state.currentRecipe;
     return (
       <div id='recipeApp'>
         <div id='recipeSideNav'>
@@ -65,7 +188,11 @@ class App extends React.Component {
             {
               this.state.recipes.map((recipe) =>
                 <li>
-                  <button onClick='changeRecipe' className='recipeBookButton'>
+                  <button
+                    id={recipe.title + '_button'}
+                    onClick={this.changeRecipe}
+                    className='recipeBookButton'
+                  >
                   {recipe.title}
                   </button>
                 </li>
@@ -74,14 +201,15 @@ class App extends React.Component {
           </ul>
         </div>
         <div id='recipeMainContent'>
-          <h1> Recipes </h1>
-          <ul>
+          {
+            currentRecipe ?
             <Recipe
-              ingredients={this.state.recipes[0].ingredients}
-              steps={this.state.recipes[0].steps}
-              title={this.state.recipes[0].title}
+              ingredients={currentRecipe.ingredients}
+              steps={currentRecipe.steps}
+              title={currentRecipe.title}
             />
-            </ul>
+            : null
+          }
         </div>
       </div>
     )
